1f9557a2fc04b7c1a0d985029e0f05ecb020d2d6
Lnx117 <vlados117@gmail.com>
2022-05-09 16:00:34 +0400

    Add db initialization. Add migrations and config files. App arch is ready

--- cmd/main.go
+++ cmd/main.go
@@ -2,19 +2,54 @@ package main
 
 import (
 	"log"
+	"os"
 	"todo"
 	"todo/pkg/handler"
 	"todo/pkg/repository"
 	"todo/pkg/service"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	repos := repository.NewRepository()
+	//Проверка на наличие файла с данными окружения
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	//Инициализация БД
+	db, err := repository.NewMysqlDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	//Внедрение зависимостей
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
+	//Запуск сервера
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while server running: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("../configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
